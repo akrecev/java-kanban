b9d2493b32bc9c22815cb75d9aa3b0d12708aec9
akrecev <akrecev@gmail.com>
2022-09-22 00:34:05 +0300

    refactor: мелкие правки после проверки ревью-6

--- src/com/yandex/practicum/managers/CSVFormatter.java
+++ src/com/yandex/practicum/managers/CSVFormatter.java
@@ -4,15 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CSVFormatter {
-    final static String SEPARATOR_FOR_READ = ",";
-    final static String SEPARATOR_FOR_WRITE = ",";
+    final static String SEPARATOR = ",";
 
     public CSVFormatter() {
     }
 
     // чтение задачи из строки
     public static Task fromString(String value) {
-        String[] data = value.split(SEPARATOR_FOR_READ);
+        String[] data = value.split(SEPARATOR);
         switch (data[1]) {
             case "TASK":
                 return new Task(TypeTask.getType(data[1]), data[2], data[4], Integer.parseInt(data[0]),
@@ -31,7 +30,7 @@ public class CSVFormatter {
     // чтение истории просмотров из строки
     public static List<Integer> historyFromString(String value) {
         List<Integer> history = new ArrayList<>();
-        String[] data = value.split(SEPARATOR_FOR_READ);
+        String[] data = value.split(SEPARATOR);
         for (String s : data) {
             history.add(Integer.parseInt(s));
         }
@@ -45,16 +44,16 @@ public class CSVFormatter {
 
     // запись задачи типа Task в строку
     public static String toString(Task task) {
-        return task.getId() + SEPARATOR_FOR_WRITE + task.getTypeTask() + SEPARATOR_FOR_WRITE
-                + task.getTitle() + SEPARATOR_FOR_WRITE + task.getStatus() + SEPARATOR_FOR_WRITE
-                + task.getDescription() + SEPARATOR_FOR_WRITE;
+        return task.getId() + SEPARATOR + task.getTypeTask() + SEPARATOR
+                + task.getTitle() + SEPARATOR + task.getStatus() + SEPARATOR
+                + task.getDescription() + SEPARATOR;
     }
 
     // запись истории просмотров в строку
     static String historyToString(HistoryManager manager) {
         StringBuilder sb = new StringBuilder();
         for (Task task : manager.getHistory()) {
-            sb.append(task.getId()).append(SEPARATOR_FOR_WRITE);
+            sb.append(task.getId()).append(SEPARATOR);
         }
         if (sb.length() > 0) {
             sb.delete(sb.length() - 1, sb.length());
